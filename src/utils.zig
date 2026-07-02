6d82e491f49e016985c30ac729c8bda6d72cd8ef
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2026-07-02 23:07:27 +0200

    Fix useless explicit math operations

--- src/utils.zig
+++ src/utils.zig
@@ -2,20 +2,16 @@ const math = @import("math.zig");
 const Vector3 = math.Vector3;
 
 pub fn triangleNormal(a: Vector3, b: Vector3, c: Vector3) Vector3 {
-    const ux = b.x - a.x;
-    const uy = b.y - a.y;
-    const uz = b.z - a.z;
-    const vx = c.x - a.x;
-    const vy = c.y - a.y;
-    const vz = c.z - a.z;
-    var nx = uy * vz - uz * vy;
-    var ny = uz * vx - ux * vz;
-    var nz = ux * vy - uy * vx;
-    const len = @sqrt(nx * nx + ny * ny + nz * nz);
+    const u = b.sub(a);
+    const v = c.sub(a);
+
+    var n = u.cross(v);
+
+    const len = n.length();
+
     if (len > 0) {
-        nx /= len;
-        ny /= len;
-        nz /= len;
+        n = n.divScalar(len);
     }
-    return .init(nx, ny, nz);
+
+    return n;
 }
